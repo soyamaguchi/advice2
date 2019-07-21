7bace66a2946e09243be311fe7ca61676c4a48aa
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-07-21 23:41:17 +0900

    vueでoverlayの実装途中

---
+++ app/javascript/packs/components/ModalContent.vue
@@ -0,0 +1,73 @@
+<template>
+    <transition name="modal" appear>
+        <div class="modal modal-overlay" @click.self="$emit('close')">
+            <div class="modal-window">
+                <div class="modal-content">
+                    <slot></slot>
+                </div>
+                <footer class="modal-footer">
+                    <slot name="footer">
+                        <a @click="$emit('close')">screenTransition</a>
+                    </slot>
+                </footer>
+            </div>
+        </div>
+    </transition>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+    /* modal */
+    .modal.modal-overlay {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        position: fixed;
+        z-index: 30;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background: rgba(0, 0, 0, 0.5);
+    }
+    .modal-window {
+        background-color: #fff;
+        border-radius: 4px;
+        overflow: hidden;
+    }
+    .modal-content {
+        padding: 10px 20px;
+    }
+    .modal-footer {
+        background-color: #ccc;
+        padding: 10px;
+        text-align: left;
+    }
+
+    /* overlay transition */
+    .modal-enter-active,
+    .modal-leave-active {
+        transition: opacity 0.3s;
+    }
+    /* overlay modalwindow transition */
+    .modal-enter-active .modal-window
+    .modal-leave-active .modal-window {
+        transition: opacity 0.3s ease 0.3s;
+    }
+    /* delay */
+    .modal-leave-active {
+        transition: opacity 0.6s ease 0.4s;
+    }
+
+    .modal-enter
+    .modal-leave-to {
+        opacity: 0;
+    }
+    .modal-enter .modal-window
+    .modal-leave-to .modal-window {
+        opacity: 0;
+        transform: translateY(-20px);
+    }
+</style>
--- app/javascript/packs/components/index.vue
+++ app/javascript/packs/components/index.vue
@@ -22,8 +22,8 @@
                             <div class="divider"></div>
                             <div class="header"><i class="tags icon"></i>カテゴリ</div>
                             <div class="scrolling menu">
-                                <div v-for="(category, i) in categories[city]" v-bind:id="i" @click="categoryContents" class="item">
-                                    <div v-bind:class="[color(i), 'ui', 'empty', 'circular', 'label']"></div>
+                                <div v-for="(category, i) in categories[city]" :id="i" @click="categoryContents" class="item">
+                                    <div :class="[color(i), 'ui', 'empty', 'circular', 'label']"></div>
                                     {{ category }}
                                 </div>
                             </div>
@@ -34,17 +34,30 @@
             <div class="row">
                 <div class="column">
                     <div class="ui link four stackable cards">
-                        <div v-for="(c, i) in contents" v-bind:id="c.city_id" v-bind:class="[color(i), 'card']">
+                        <div v-for="(c, i) in contents" :id="'isModalActive' + i" :class="[color(i), 'card']" @click="openModalContent">
                             <div class="image center aligned">
                                 <font-awesome-icon icon="graduation-cap" size="5x" class="fai"/>
                             </div>
                             <div class="content main-title">
                                 <div class="header">{{ c.main_title }}</div>
-                                <div class="meta" v-bind:id="c.category_id"><span class="category">{{ c.category_name }}</span></div>
+                                <div class="meta"><span class="category">{{ c.category_name }}</span></div>
                             </div>
                             <div class="extra content">
                                 <span><i class="sync alternate icon"></i>更新日：{{ c.update_time }}</span>
                             </div>
+
+                            <ModalContent @close="closeModalContent" v-if="ModalActiveArr.isModalActive0">
+                                <p>Vue.js Modal Window!</p>
+                                <div>TestSample</div>
+                                <template slot="footer">
+                                    <p>下記の出典元情報を加工して掲載しております</p>
+                                    <a :href="c.url">出典：{{ c.prefecture }} {{ c.city }}{{ c.ward }}のサイト</a>
+                                </template>
+                            </ModalContent>
+
+<!--                            <div :class="['modal', {'active': isModalActive}]">-->
+<!--                                modal test-->
+<!--                            </div>-->
                         </div>
                     </div>
                 </div>
@@ -54,6 +67,7 @@
 </template>
 
 <script>
+    import ModalContent from './ModalContent';
     import axios from 'axios';
     import Qs from "qs";
     axios.defaults.headers.common = {
@@ -62,6 +76,9 @@
     };
 
     export default {
+        components: {
+          ModalContent
+        },
         data: function() {
             return {
                 /** semantic ui colors */
@@ -69,6 +86,9 @@
                 /** categories */
                 categories: {
                     1: ["行政オンラインサービス", "妊娠・出産", "子育て", "教育", "結婚・離婚", "引越し・住まい", "就職・退職", "介護・高齢者", "ご不幸", "台風21号関連", "大阪府北部を震源とする地震関連", "安全・安心", "戸籍・住民票・印鑑登録", "税", "国民健康保健", "国民年金", "水道・ガス・電気", "交通", "駐輪・駐車", "都市計画", "ごみ・環境保全", "食品・衛生", "ペット・動物", "消費生活", "健康・医療・福祉", "文化・スポーツ・生涯学習", "市民活動"],
+                },
+                /** modal activeflag */
+                ModalActiveArr: {
                 },
                 /** queryparameter */
                 queryParams: {
@@ -95,17 +115,28 @@
         methods: {
             getContents: function() {
                 axios.get('api/contents', { params: { q: this.queryParams }, paramsSerializer: (params) => Qs.stringify(params, { arrayFormat: 'brackets' }) }).then((res) => {
+                    [...Array(this.contents.length)].forEach((_, i) => this.$delete(this.ModalActiveArr, "isModalActive" + i))
                     this.contents.splice(0)
-                    res.data.contents.forEach(v => this.contents.push(v))
+                    res.data.contents.forEach((v, i) => {
+                        this.contents.push(v)
+                        this.$set(this.ModalActiveArr, "isModalActive" + i, false)
+                    })
                     this.city = (this.contents.length > 0) ? this.contents[0].city_id : 1
                 }, (err) => {
-                    this.contents.length = 0
+                    this.contents.splice(0)
                     console.log(err)
                 });
             },
             categoryContents: function(e) {
                 this.$set(this.queryParams, "category", e.target.id)
                 this.getContents()
+            },
+            openModalContent: function(e) {
+                const contentDom = e.target.offsetParent || e.target.parentElement.offsetParent || e.target.parentElement.parentElement.offsetParent
+                this.ModalActiveArr[contentDom.id] = true
+            },
+            closeModalContent: function() {
+                // this.isModalActive
             },
             color: function(i) {
                 const colorLength = this.colorArr.length
@@ -117,7 +148,7 @@
 
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
     /* 全体 */
     [v-cloak] {
         display: none;
@@ -157,6 +188,4 @@
         margin-left: 3px;
         margin-top: 3px;
     }
-
-
 </style>
