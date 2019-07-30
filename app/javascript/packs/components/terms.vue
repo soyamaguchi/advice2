e1417ad077968c3c8c3c119a278b9fc217e18f36
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-07-30 23:29:07 +0900

    footerに載せるページのファイルやvueルーティングの作成

---
+++ app/javascript/packs/components/terms.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+    export default {
+        name: "terms"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- app/javascript/packs/router/router.js
+++ app/javascript/packs/router/router.js
@@ -1,12 +1,16 @@
 import Vue from 'vue/dist/vue.esm'
 import VueRouter from 'vue-router'
 import Index from '../components/index'
+import Terms from '../components/terms'
+import Privacy from '../components/privacy'
 
 Vue.use(VueRouter)
 
 export default new VueRouter({
     mode: 'history',
     routes: [
-        { path: '/', component: Index }
+        { path: '/', component: Index },
+        { path: '/terms', component: Terms },
+        { path: '/privacy', components: Privacy },
     ],
 })
