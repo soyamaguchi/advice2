2a27708ff8b8100c1202c320d873b94971e6206d
Soya Yamaguchi <f.soya.maguchi@gmail.com>
2019-08-25 21:52:38 +0900

    api spec test

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :category do
-      sequence(:id) { |i| i }
+      sequence(:id, 1) { |i| i }
       city_id { 1 }
       sequence(:category_id, 1) { |i| i }
       category_name { category_id == 1 ? "行政オンラインサービス" : category_id == 2 ? "妊娠・出産" : category_id == 3 ? "子育て" : "教育" }
--- spec/factories/contents.rb
+++ spec/factories/contents.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :content do
-    sequence(:id, 1) { |i| i }
+    sequence(:id, 100) { |i| i }
     city_id { 1 }
     url { "https://www.city.osaka.lg.jp" }
     sequence(:category, 1) { |i| i }
